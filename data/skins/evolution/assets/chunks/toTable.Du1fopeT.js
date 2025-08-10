d488f38f5f182d0459854a5a8052b314d7efc8d1
iSundram <abhayshankart1@gmail.com>
2025-08-10 03:06:37 +0000

    Added ./data/skins/evolution/assets/chunks/toTable.Du1fopeT.js

---
+++ data/skins/evolution/assets/chunks/toTable.Du1fopeT.js
@@ -0,0 +1,1 @@
+import{dc as r,a4 as a,bK as m}from"../index.DtGSrwJ3.js";const u=t=>o=>{const{info:s}=o,n=r(["info"],o);return{columns:s.columns,rowsCount:Number(s.rows),rows:a(t,m(n))}};export{u as t};
