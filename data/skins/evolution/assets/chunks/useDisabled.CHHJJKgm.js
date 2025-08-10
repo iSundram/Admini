7793d90c2f7fab53a8d476cef4ecf2d981bbf6cd
iSundram <abhayshankart1@gmail.com>
2025-08-10 03:12:08 +0000

    Added ./data/skins/evolution/assets/chunks/useDisabled.CHHJJKgm.js

---
+++ data/skins/evolution/assets/chunks/useDisabled.CHHJJKgm.js
@@ -0,0 +1,1 @@
+import{bU as o,n as r,c as t}from"../index.DtGSrwJ3.js";const i=s=>{const e=o("form-disabled",r(!1)),a=t(()=>e.value||s.value);return Object.freeze({formDisabled:e,isDisabled:a})};export{i as u};
