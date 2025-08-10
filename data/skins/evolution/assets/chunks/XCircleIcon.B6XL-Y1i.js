c0f0eb4efc941f3abce84c2bc6fc6af42bc4708d
iSundram <abhayshankart1@gmail.com>
2025-08-10 03:09:06 +0000

    Added ./data/skins/evolution/assets/chunks/XCircleIcon.B6XL-Y1i.js

---
+++ data/skins/evolution/assets/chunks/XCircleIcon.B6XL-Y1i.js
@@ -0,0 +1,1 @@
+import{o as e,b as r,e as o}from"../index.DtGSrwJ3.js";function s(t,n){return e(),r("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor","aria-hidden":"true"},[o("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"})])}export{s as r};
